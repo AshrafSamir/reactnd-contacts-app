bc42f8740ea07f2a2d435cec5edb8edf472305b6
AshrafSamir <36134927+AshrafSamir@users.noreply.github.com>
2020-06-13 20:16:53 +0200

    feat: ListContact and CreatContact component added

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React, { Component } from 'react'
-import ListContacts from './ListContacts'
-import * as ContactsAPI from './utils/ContactsAPI'
-import CreateContact from './CreateContact'
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import ListContacts from "./ListContacts";
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreatContact from "./CreateContact";
+
+
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts:  [],
+    screen: 'list'
   }
   componentDidMount() {
-    ContactsAPI.getAll()
-      .then((contacts) => {
-        this.setState(() => ({
-          contacts
-        }))
+    ContactsAPI.getAll().then((contacts) =>{
+      this.setState({
+        contacts: contacts
       })
+    })
   }
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id
-      })
-    }))
 
+  removeContact = (contact) => {
+    this.setState((currState) =>({
+      contacts: currState.contacts.filter((c) => {return c.id !== contact.id})
+    }) )
     ContactsAPI.remove(contact)
   }
   createContact = (contact) => {
     ContactsAPI.create(contact)
-      .then((contact) => {
-        this.setState((currentState) => ({
-          contacts: currentState.contacts.concat([contact])
-        }))
-      })
+        .then((contact) => {
+          this.setState((currState)=>({
+            contacts: currState.contacts.concat([contact])
+          }))
+        })
+
   }
   render() {
     return (
       <div>
         <Route exact path='/' render={() => (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
+            <ListContacts
+            contacts = {this.state.contacts}
+            onDeleteContact = {this.removeContact}
+        />)
+        }/>
+        <Route path='/create' render={({history})=>(
+          <CreatContact
+              onCreateContact={(contact) => {
+                this.createContact(contact)
+                history.push('/')
+              }}
           />
-        )} />
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )} />
+        )}/>
       </div>
-    )
+
+  );
   }
 }
 
-export default App
+export default App;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,80 +1,82 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import  PropTypes from "prop-types"
+import { Link } from "react-router-dom";
 
-class ListContacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired,
-  }
-  state = {
-    query: ''
-  }
-  updateQuery = (query) => {
-    this.setState(() => ({
-      query: query.trim()
-    }))
-  }
-  clearQuery = () => {
-    this.updateQuery('')
-  }
-  render() {
-    const { query } = this.state
-    const { contacts, onDeleteContact } = this.props
 
-    const showingContacts = query === ''
-      ? contacts
-      : contacts.filter((c) => (
-          c.name.toLowerCase().includes(query.toLowerCase())
-        ))
+class  ListContacts extends Component{
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired,
+    }
+    state = {
+        query: '',
+    }
 
-    return (
-      <div className='list-contacts'>
-        <div className='list-contacts-top'>
-          <input
-            className='search-contacts'
-            type='text'
-            placeholder='Search Contacts'
-            value={query}
-            onChange={(event) => this.updateQuery(event.target.value)}
-          />
-          <Link
-            to='/create'
-            className='add-contact'
-          >Add Contact</Link>
-        </div>
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+    clearQuerry = () => {
+        this.updateQuery('')
+    }
+    render() {
 
-        {showingContacts.length !== contacts.length && (
-          <div className='showing-contacts'>
-            <span>Now showing {showingContacts.length} of {contacts.length}</span>
-            <button onClick={this.clearQuery}>Show all</button>
-          </div>
-        )}
+        const { query } = this.state
+        const {contacts, onDeleteContact} = this.props
 
-        <ol className='contact-list'>
-          {showingContacts.map((contact) => (
-            <li key={contact.id} className='contact-list-item'>
-              <div
-                className='contact-avatar'
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}
-              ></div>
-              <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.handle}</p>
-              </div>
-              <button
-                onClick={() => onDeleteContact(contact)}
-                className='contact-remove'>
-                  Remove
-              </button>
-            </li>
-          ))}
-        </ol>
-      </div>
-    )
-  }
+        const showingContacts = query == '' ? contacts :
+            contacts.filter((c) => (c.name.toLowerCase().includes(query.toLowerCase())))
+
+        return(
+            <div className="list-contacts">
+                <div className="list-contacts-top">
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                    <Link className="add-contact"
+                       to="/create">Add contact</Link>
+                </div>
+                {
+                    showingContacts.length !== contacts.length && (
+                        <div className="showing-contacts">
+                            <span>Now showing {showingContacts.length} of {contacts.length}</span>
+                            <button onClick={this.clearQuerry}>Show all</button>
+
+                        </div>
+                    )
+                }
+                <ol className='contact-list'>
+                    {showingContacts.map((contact) => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div
+                                className='contact-avatar'
+                                style={{ backgroundImage:`url(${contact.avatarURL})`
+                                }}
+                            ></div>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button className={'contact-remove'}
+                                    onClick={() => {onDeleteContact(contact)}}
+                            >
+                                remove
+                            </button>
+
+                        </li>
+                    ))}
+                </ol>
+            </div>
+
+        )
+    }
 }
+
+
 
 export default ListContacts
